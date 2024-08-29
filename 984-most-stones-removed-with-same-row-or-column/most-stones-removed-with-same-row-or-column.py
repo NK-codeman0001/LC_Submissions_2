5625cd8be2e9d15c7b712161d414187dafbf0b7f
Neeraj Kumar <66253834+NK-codeman0001@users.noreply.github.com>
2024-08-29 15:17:04 +0530

    Time: 98 ms (94.67%) | Memory: 12.4 MB (78.67%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def removeStones(self, stones):
+        """
+        :type stones: List[List[int]]
+        :rtype: int
+        """
+        # To store the graph connections
+        graph = {}
+        
+        # Build the graph
+        for x, y in stones:
+            if x not in graph:
+                graph[x] = []
+            if ~y not in graph:
+                graph[~y] = []
+            graph[x].append(~y)
+            graph[~y].append(x)
+        
+        # To track visited nodes
+        visited = set()
+        
+        def dfs(node):
+            stack = [node]
+            while stack:
+                current = stack.pop()
+                if current not in visited:
+                    visited.add(current)
+                    for neighbor in graph[current]:
+                        if neighbor not in visited:
+                            stack.append(neighbor)
+        
+        # Counting connected components
+        components = 0
+        for x, y in stones:
+            if x not in visited:
+                dfs(x)
+                components += 1
+        
+        # The number of stones that can be removed is total stones minus the number of connected components
+        return len(stones) - components
+        
